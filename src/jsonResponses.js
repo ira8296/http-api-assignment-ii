c68b170a00bf7d5e72003dbc4efa73b414da8dea
Ike Ajah (RIT Student) <ira8296@rit.edu>
2020-09-20 20:24:56 -0400

    Revision needed

---
+++ src/jsonResponses.js
@@ -0,0 +1,98 @@
+const users = {};
+
+const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  // set status code and content type (application/json)
+  response.writeHead(status, headers);
+  // stringify the object (so it doesn't use references/pointers/etc)
+  // but is instead a flat string object.
+  // Then write it to the response.
+  response.write(JSON.stringify(object));
+  // Send the response to the client
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.end();
+};
+
+const addUser = (request, response, params) => {
+  const newUser = {
+    createdAt: Date.now(),
+  };
+
+  if (!params.name || !params.age) {
+    const responseJSON = {
+      id: 'missingParams',
+      message: 'Name and age are both required',
+    };
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  if (users[params.name]) {
+    //users[params.name] = {};
+    users[params.name].name = params.name;
+    users[params.name].age = params.age;
+    // users[params.name] = newUser;
+    return respondJSONMeta(request, response, 204);
+  }
+
+  newUser.name = params.name;
+  newUser.age = params.age;
+  newUser.message = 'Created successfully';
+  users[newUser.createdAt] = newUser;
+
+  return respondJSON(request, response, 201, newUser);
+};
+
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const notReal = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for does not exist',
+    id: 'notFound',
+  };
+  respondJSON(request, response, 404, responseJSON);
+};
+
+const notRealMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
+};
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for could not be found',
+    id: 'notFound',
+  };
+  respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
+};
+
+module.exports = {
+  addUser,
+  getUsers,
+  getUsersMeta,
+  notReal,
+  notRealMeta,
+  notFound,
+  notFoundMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,66 @@
+const http = require('http'); // pull in the http server module
+const url = require('url'); // pull in the url module
+// pull in the query string module
+const query = require('querystring');
+// pull in our html response handler file
+const htmlHandler = require('./htmlResponses.js');
+// pull in our json response handler file
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getStyle,
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notReal,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    '/notReal': jsonHandler.notRealMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const params = [];
+
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
+
+    request.on('data', (chunk) => {
+      params.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(params).toString();
+      const bodyParams = query.parse(bodyString);
+
+      jsonHandler.addUser(request, response, bodyParams);
+    });
+  }
+};
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+  const parsedUrl = url.parse(request.url);
+  // const params = query.parse(parsedUrl.query);
+
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
